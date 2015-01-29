d73f044bf09590859d80fe43cd0e23c4495d9c84
Joel Pickup <joelpickup@hotmail.co.uk>
2015-01-29 21:42:04 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var app = express();
+var Twit = require('twit');
+
+// Listen for incoming requests and serve them.
+app.listen(process.env.PORT || 3000);
+
+app.set('views', './views');
+app.set('view engine', 'jade');
+
+var twitter = new Twit({
+  consumer_key: process.env.TWITTER_API_KEY,
+  consumer_secret: process.env.TWITTER_API_SECRET,
+  access_token: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET
+});
+
+var tweets;
+twitter.get('users/show', { screen_name: 'joel' },  function (err, data, response) {
+  tweets = data;
+});
+
+
+// Generate a simple home page.
+app.get('/', function(req, res) {
+  var tweet = {};
+  tweet.user = tweets;
+  res.render('index', { data: JSON.stringify(tweets) });
+});
