b14fdc7c35b6328bd542ad3243c52bc34f38f116
Joel Pickup <joelpickup@hotmail.co.uk>
2015-01-30 10:46:16 +0000

    included css files onto the jade fight page

--- game.js
+++ game.js
@@ -5,5 +5,15 @@ $(document).ready(function(){
   });
   $('#Fight').on('click', function(){
     console.log("fight");
+    tweets1 = parseInt($('#tweets1').text());
+    tweets2 = parseInt($('#tweets2').text());
+    followers1 = parseInt($('#followers1').text());
+    followers2 = parseInt($('#followers2').text());
+    favourites1 = parseInt($('#favourites1').text());
+    favourites2 = parseInt($('#favourites2').text());
+
+
+
+
   });
 });
